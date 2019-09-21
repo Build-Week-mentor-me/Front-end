1058e332c4aeef5a4779cdcd9762c1f18a6a6bac
Cobrettie <cobrettiegarner3@gmail.com>
2019-09-21 10:53:47 -0400

    basic questionform component built, added to AppRouting component for react-router, imported AppRouting into App.js

--- mentor-me/src/App.js
+++ mentor-me/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavMenu from './components/NavMenu/NavMenu';
+import AppRouting from './components/AppRouting';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <NavMenu />
+      <AppRouting />
     </div>
   );
 }
--- mentor-me/src/components/AppRouting.js
+++ mentor-me/src/components/AppRouting.js
@@ -1,10 +1,13 @@
-// import React from 'react';
-// import { Route } from 'react-router-dom';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import FormikQuestionForm from './Questions/QuestionForm';
 
-// const AppRouting = () => {
-//     return (
+const AppRouting = () => {
+    return (
+        <div>
+            <Route exact path="/questionform" component={FormikQuestionForm} />
+        </div>
+    )
+}
 
-//     )
-// }
-
-// export default AppRouting
+export default AppRouting
