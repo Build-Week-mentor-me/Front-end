d890e88e946d46751d9108dea38b25b5cd2d0406
Cobrettie <cobrettiegarner3@gmail.com>
2019-09-21 17:20:49 -0400

    update styling for navmenu logo, mainUl

--- mentor-me/src/components/AppRouting.js
+++ mentor-me/src/components/AppRouting.js
@@ -5,9 +5,13 @@ import FormikQuestionForm from '../components/Questions/QuestionForm';
 const AppRouting = () => {
     return (
         <div>
-            <Route exact path="/questionform" component={FormikQuestionForm} />
+            <Route 
+                exact path="/questionform" 
+                component={FormikQuestionForm} 
+            />
+            <h1>Hello from approuting</h1>
         </div>
     )
 }
 
-export default AppRouting
+export default AppRouting;
