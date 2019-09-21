4114d8e03056802708bcb4c55a8d2bebf315deca
Cobrettie <cobrettiegarner3@gmail.com>
2019-09-21 16:26:41 -0400

    begin styling navmenu to match marketing page

--- mentor-me/src/components/AppRouting.js
+++ mentor-me/src/components/AppRouting.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import FormikQuestionForm from './Questions/QuestionForm';
+import FormikQuestionForm from '../components/Questions/QuestionForm';
 
 const AppRouting = () => {
     return (
