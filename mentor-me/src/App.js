4d7124c4ac034f94a38025282da7e76a0662988e
Cobrettie <cobrettiegarner3@gmail.com>
2019-09-21 17:00:54 -0400

    basic footer component built

--- mentor-me/src/App.js
+++ mentor-me/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavMenu from './components/NavMenu/NavMenu';
 import AppRouting from './components/AppRouting';
+import Footer from './components/Footer/Footer';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <NavMenu />
       <AppRouting />
+      <Footer />
     </div>
   );
 }
