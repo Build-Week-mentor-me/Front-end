b20e57cdb3f573f0665daac6999720f097d963b3
Cobrettie <cobrettiegarner3@gmail.com>
2019-09-24 20:40:57 -0400

    begin building Questions component that will display all questions in the database

--- mentor-me/src/Components/Questions/Questions.js
+++ mentor-me/src/Components/Questions/Questions.js
@@ -1,2 +1,12 @@
 import React from 'react';
+import axios from 'axios';
 
+const Questions = (props) => {
+    return (
+        <section>
+            <h1>Question List</h1>
+        </section>
+    )
+}
+
+export default Questions
